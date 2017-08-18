4bf894b7d176ad63c0bfa96972d32e568d312073
Andrey <asmishurin@gmail.com>
2017-08-18 11:48:02 +0300

    User registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'posts#index'
   resources :posts
   resources :pictures, only: [:create, :destroy]
   resources :tags, only: [:show]
-  resources :categories
+  resources :categories, only: [:show]
+
+  namespace :admin do
+    resources :categories, except: [:show]
+   end
 end
 
